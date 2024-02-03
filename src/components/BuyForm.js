b25b8bb14b2d621c20643996f07778f879401272
MichaelHDesigns <[devilking6105@gmail.com]>
2024-02-03 02:17:48 -0500

    figuring out this buy function

--- src/components/BuyForm.js
+++ src/components/BuyForm.js
@@ -1,49 +1,53 @@
-import React, { Component } from 'react'
-import tokenLogo from '../token-logo.png'
-import ethLogo from '../eth-logo.png'
-
+import React, { Component } from 'react';
+import tokenLogo from '../token-logo.png';
+import ethLogo from '../eth-logo.png';
 
 class BuyForm extends Component {
-
-  constructor(props){
-super(props)
-this.state ={
-  output: '0'
-}
-}
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      output: '0',
+    };
+  }
 
   render() {
     return (
-        <form className="mb-3" onSubmit={(event) => {
-            event.preventDefault()
-            let etherAmount
-            etherAmount = this.input.value.toString()
-            etherAmount = window.web3.utils.toWei(etherAmount, 'Ether')
-            this.props.buyTokens(etherAmount)
-                  }}>
+      <form
+        className="mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          const etherAmount = this.state.input.trim();
+          if (!etherAmount) return; // Add validation for empty input
+
+          // Pass the entered amount directly without converting to Wei
+          this.props.buyTokens(etherAmount);
+        }}
+      >
         <div>
           <label className="float-left"><b>Input</b></label>
           <span className="float-right text-muted">
-            Balance:{window.web3.utils.fromWei(this.props.etherBalance,'Ether')}
+            Balance: {window.web3.utils.fromWei(this.props.etherBalance, 'Ether')}
           </span>
         </div>
         <div className="input-group mb-4">
           <input
-onChange={(event) => {
-  const etherAmount = event.target.value;
-  this.setState({
-    output: etherAmount,
-  });
-}}
-            ref={(input) =>  {this.input = input}}
+            onChange={(event) => {
+              const etherAmount = event.target.value;
+              this.setState({
+                input: etherAmount,
+                output: etherAmount,
+              });
+            }}
+            ref={(input) => { this.input = input; }}
             type="text"
             className="form-control form-control-lg"
             placeholder="0"
-            required />
+            required
+          />
           <div className="input-group-append">
             <div className="input-group-text">
-              <img src={ethLogo} height='32' alt=""/>
+              <img src={ethLogo} height="32" alt="" />
               &nbsp;&nbsp;&nbsp; HTH
             </div>
           </div>
@@ -51,20 +55,20 @@ onChange={(event) => {
         <div>
           <label className="float-left"><b>Output</b></label>
           <span className="float-right text-muted">
-            Balance: {window.web3.utils.fromWei(this.props.tokenBalance,'Ether')}
-               </span>
+            Balance: {window.web3.utils.fromWei(this.props.tokenBalance, 'Ether')}
+          </span>
         </div>
         <div className="input-group mb-2">
           <input
             type="text"
-className="form-control form-control-lg"
+            className="form-control form-control-lg"
             placeholder="0"
-          value= {this.state.output}
+            value={this.state.output}
             disabled
           />
           <div className="input-group-append">
             <div className="input-group-text">
-              <img src={tokenLogo} height='32' alt=""/>
+              <img src={tokenLogo} height="32" alt="" />
               &nbsp; wHTH
             </div>
           </div>
@@ -75,9 +79,9 @@ className="form-control form-control-lg"
         </div>
         <button type="submit" className="btn btn-primary btn-block btn-lg">SWAP!</button>
       </form>
-
     );
   }
 }
 
 export default BuyForm;
+
